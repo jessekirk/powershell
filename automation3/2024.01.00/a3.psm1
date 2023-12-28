f0e83c0f916ba75f1fee89855846559973f17337
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2023-12-28 16:51:47 -0600

    initial commit

---
+++ automation3/2024.01.00/a3.psm1
@@ -0,0 +1,39 @@
+$script:json = Get-Content './a3.json' | ConvertFrom-Json
+Add-Type -Path $json.ionicZipDllPath
+
+function a3DateTimeUtcNow
+{
+    return (Get-Date).ToUniversalTime().ToString('yyyyMMddTHHmmZ')
+}
+
+function a3ConvertString256Hash
+{
+    [cmdletbinding()]
+    param([string]$p)
+
+    $sb = New-Object System.Text.StringBuilder
+    [System.Security.Cryptography.HashAlgorithm]::Create('SHA256').ComputeHash([System.Text.Encoding]::UTF8.GetBytes($p)) | ForEach-Object {
+        [void]$sb.Append($_.ToString('x2'))
+    }
+
+    return $sb.ToString().Substring(0, 5)
+}
+
+function a3GitPath
+{
+    if ((Get-CimInstance -ClassName Win32_LogicalDisk).DeviceID -match 'd:') { return 'd:\a3Temp' }
+    return "$env:USERPROFILE\a3Temp"
+}
+
+function a3CreateNewPkg
+{
+    [cmdletbinding()]
+    param
+    (
+        [parameter(Mandatory)]
+        [ValidateScript( { Test-Path -Path $_ })]
+        [string]$path
+    )
+
+
+}
